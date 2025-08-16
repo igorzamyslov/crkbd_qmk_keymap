356b9f3a54d7562d8d4c1c9d99421f013e90df87
Igor Zamyslov <zamylovigor@gmail.com>
2025-08-16 10:22:43 +0200

    Fix compilation script

--- compile_crkbd.sh
+++ compile_crkbd.sh
@@ -1,8 +1,12 @@
-set -euo pipefail
+#!/bin/bash
+set -xeuo pipefail
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
+QMK_FIRMWARE_DIR="${HOME}"/Projects/qmk_firmware
 
-qmk json2c "${SCRIPT_DIR}/_keymap.json" -o "${SCRIPT_DIR}/keymap.c"
+qmk json2c "${SCRIPT_DIR}"/_keymap.json -o "${SCRIPT_DIR}"/keymap.c
 qmk compile --clean -kb crkbd/rev4_1/standard -km miryoku_based
-mkdir -p out
-mv *.uf2 out/
+
+mkdir -p "${SCRIPT_DIR}"/out
+mv "${QMK_FIRMWARE_DIR}"/*.uf2 "${SCRIPT_DIR}"/out/
+cp "${SCRIPT_DIR}"/out/*.uf2 /System/Volumes/Data/Volumes/RPI-RP2
